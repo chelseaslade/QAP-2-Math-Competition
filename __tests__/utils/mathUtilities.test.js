df40d188d28ae38baa53a7247292ab4709d82917
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-15 20:41:27 -0230

    Correct/incorrect tests

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,23 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-describe("Tests for getQuestion", () => {
-  test("Function output", () => {
-    expect();
+//Test for generating a question
+// describe("Tests for getQuestion", () => {
+//   test("Function output", () => {
+//     expect();
+//   });
+// });
+
+//Test for correct & incorrect answers
+describe("Tests for isCorrectAnswer", () => {
+  test("Returns true for correct answer", () => {
+    const question = { correctAnswer: 5 };
+    const answer = 5;
+    expect(isCorrectAnswer(question, answer)).toBe(true);
+  });
+
+  test("Return false for incorrect answer", () => {
+    const question = { correctAnswer: 5 };
+    const answer = 3;
+    expect(isCorrectAnswer(question, answer)).toBe(false);
   });
 });
-
-describe("Tests for isCorrectAnswer", () => {});
