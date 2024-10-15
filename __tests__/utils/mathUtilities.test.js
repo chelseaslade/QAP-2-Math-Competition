e7ca4fd632ca6131116696cc4b4ddc09b1d37627
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-15 21:00:02 -0230

    Test getQuestion

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,11 +1,37 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
 //Test for generating a question
-// describe("Tests for getQuestion", () => {
-//   test("Function output", () => {
-//     expect();
-//   });
-// });
+describe("Tests for getQuestion", () => {
+  test("Ensure operator is one of symbols in list", () => {
+    const result = getQuestion();
+    const operator = result.question.split(" ")[1];
+
+    expect(["+", "-", "*", "/"]).toContain(operator);
+  });
+
+  test("Check that correctAnswer is result of correctAnswer calculation", () => {
+    const result = getQuestion();
+    const [x, symbol, y] = result.question.split(" ");
+    const correctAnswer = result.correctAnswer;
+
+    let expectedAnswer = "";
+    switch (symbol) {
+      case "+":
+        expectedAnswer = parseInt(x) + parseInt(y);
+        break;
+      case "-":
+        expectedAnswer = parseInt(x) - parseInt(y);
+        break;
+      case "*":
+        expectedAnswer = parseInt(x) * parseInt(y);
+        break;
+      case "/":
+        expectedAnswer = parseInt(x) / parseInt(y);
+        break;
+    }
+    expect(correctAnswer).toBe(expectedAnswer);
+  });
+});
 
 //Test for correct & incorrect answers
 describe("Tests for isCorrectAnswer", () => {
